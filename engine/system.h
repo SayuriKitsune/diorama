6309ee9ebcd529d98ebed31d596f1b611887fb8a
SayuriKitsune <wolfcoderws@gmail.com>
2017-09-06 15:41:46 -0600

    Added fixed point numbers

--- engine/system.h
+++ engine/system.h
@@ -1,6 +1,17 @@
 #ifndef SYSTEM_H
 #define SYSTEM_H
 
+/* Fixed point numbers */
+typedef int fint;
+#define FINT_ONE 256
+#define FINT_ADD(a,b) ((a)+(b))
+#define FINT_SUB(a,b) ((a)-(b))
+#define FINT_MUL(a,b) (((a)*(b))>>8)
+#define FINT_TO_INT(a) ((a)>>8)
+#define FINT_TO_FLOAT(a) (((float)a)/256.0f)
+#define FINT_FROM_INT(a) ((a)<<8)
+#define FINT_FROM_FLOAT(a) ((int)((a)*256.0f))
+
 /* System */
 namespace System
 {
